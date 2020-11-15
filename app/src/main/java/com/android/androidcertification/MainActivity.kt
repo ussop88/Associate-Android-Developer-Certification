c7e0b989a7d8a586c0052bc03e3aa20ac0bb7b1e
Youssef MOURCHID <mourchid.youssef@gmail.com>
2020-11-15 18:26:20 +0100

    show toast

--- app/src/main/java/com/android/androidcertification/MainActivity.kt
+++ app/src/main/java/com/android/androidcertification/MainActivity.kt
@@ -2,10 +2,24 @@ package com.android.androidcertification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private val duration = Toast.LENGTH_SHORT
+    private val message = "this is hte toast"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        showToast()
+    }
+
+    private fun showToast() {
+        Toast.makeText(this, message, duration)
     }
 }
